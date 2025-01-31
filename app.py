95c845156f5bf8e80fada4bb86348ae228c60f74
sisco1206 <fnahounou6@gmail.com>
2025-01-31 00:15:37 -0600

    modified app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # for loop
 
-bot_names = ["Jeff", "Paul", "Chris"]
+'''bot_names = ["Jeff", "Paul", "Chris"]
 for name in bot_names:
     print(name)
 
@@ -23,4 +23,31 @@ def add_numbers(a,b):
 def get_todays_date():
     return datetime.now().strftime("%m/%d/%Y")
 
-print(get_todays_date())
+print(get_todays_date())'''
+
+# my first chatbot
+from datetime import datetime
+
+name = "jeff"
+
+def add(a, b):
+    return a + b
+
+def get_todays_date():
+    return datetime.now().strftime("%m/%d/%Y")
+
+while True:
+    user_message = input("Bot - How can I help you today? ").strip().lower()
+
+    if user_message == "what is your name?":
+        print("Bot - My name is", name)
+
+    elif user_message == "what is the date?":
+        print("Bot - Today's date is", get_todays_date())
+
+    elif user_message == "exit":
+        print("Bot - Goodbye!")
+        break  # Exits the loop
+
+    else:
+        print("Bot - I'm sorry, I don't understand that.")
